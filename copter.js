fd0491313e6616c97e94e09f4072cee9836e8e6a
sarajo <sarajchipps@gmail.com>
2015-04-16 17:51:33 -0400

    testing functions

--- copter.js
+++ copter.js
@@ -1,3 +1,18 @@
+function whatever(){
+    client.after(4000, function(){
+        this.animate('turnaround', 3000);
+    });
+    client.after(1000, function(){
+        this.animate('wave', 4000);
+    });
+    client.after(1000, function(){
+        this.animate('turnaround', 3000);
+    });
+    client.after(1000, function(){
+        this.animate('doublePhiThetaMixed', 8000);
+    });
+};
+
 function groove(){
 	client.after(2000, function(){
 		this.left(0.1);
@@ -31,7 +46,85 @@ function groove(){
 		this.stop();
 	});
 }
+function cha_cha(){
+  client.after(2000, function(){
+    this.animate('thetaDance', 1000);
+  });
+    client.after(2000, function(){
+    this.stop();
+  });
+  client.after(1000, function(){
+    this.animate('yawDance', 1000);
+  });
+  client.after(1000, function(){
+    this.animate('flipLeft', 1000);
+  });
+  client.after(2000, function(){
+    this.stop();
+  });
+    client.after(1000, function(){
+    this.animate('flipRight', 1000);
+  });
+}
 
+function twirly_twist(){
+    client.after(2000, function(){
+        this.animate('yawDance', 1000);
+    });
+    client.after(1000, function(){
+        this.animate('turnaround', 1000);
+    });
+    client.after(1000, function(){
+        this.animate('yawDance', 1000);
+    });
+}
+
+function grapevine(){
+
+client.after(1000, function(){
+	this.right(0.1);
+});
+
+client.after(1000, function(){
+
+	this.stop();
+});
+
+
+client.after(1000, function(){
+
+	this.left(0.1);
+});
+
+client.after(1000, function(){
+
+	this.stop();
+});
+
+
+client.after(1000, function(){
+
+	this.front(0.1);
+});
+client.after(1000, function(){
+
+	this.stop();
+});
+
+client.after(1000, function(){
+	this.back(0.1);
+});
+client.after(1000, function(){
+
+	this.stop();
+});
+
+client.after(1000, function(){
+
+	this.animate("wave", 4000);
+});
+
+};
 
 var arDrone = require('ar-drone');
 var client = arDrone.createClient();
@@ -39,10 +132,11 @@ var client = arDrone.createClient();
 client.takeoff();
 
 groove();
-client.after(2000, function(){
-	this.animate('wave', 4000);
-});
-groove();
+twirly_twist();
+cha_cha();
+grapevine();
+whatever();
+
 client.after(2000, function(){
   this.land();
 });
